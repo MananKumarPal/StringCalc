9a51c727c8ad6622b40dbcc06f976d3c7712be26
Manan-kumar-pal <>
2025-07-15 01:11:01 +0530

    Green: Handle newlines between numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,7 +2,9 @@ class StringCalculator {
   add(numbers) {
     if (numbers === "") return 0;
 
-    const numbersArray = numbers.split(",");
+    //
+    const cleanNumbers = numbers.replace(/\n/g, ",");
+    const numbersArray = cleanNumbers.split(",");
     let sum = 0;
 
     for (let num of numbersArray) {

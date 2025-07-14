38c22749130e4d7cd91a21dca992f22b1569c833
Manan-kumar-pal <>
2025-07-15 01:12:24 +0530

    Green: Support custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,16 +2,35 @@ class StringCalculator {
   add(numbers) {
     if (numbers === "") return 0;
 
-    //
-    const cleanNumbers = numbers.replace(/\n/g, ",");
+    let delimiter = ",";
+    let numberString = numbers;
+
+    // Check for custom delimiter
+    if (numbers.startsWith("//")) {
+      const delimiterLine = numbers.split("\n")[0];
+      delimiter = delimiterLine.substring(2); // Remove '//'
+      numberString = numbers.split("\n")[1]; // Get numbers part
+    }
+
+    // Replace newlines and custom delimiters with commas
+    const cleanNumbers = numberString
+      .replace(/\n/g, ",")
+      .replace(new RegExp(this.escapeRegex(delimiter), "g"), ",");
+
     const numbersArray = cleanNumbers.split(",");
     let sum = 0;
 
     for (let num of numbersArray) {
-      sum += parseInt(num);
+      if (num !== "") {
+        sum += parseInt(num);
+      }
     }
 
     return sum;
+  }
+
+  escapeRegex(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
   }
 }
 

c15479d8b40c76b9f13478025fc5660e35e5d5ac
Manan-kumar-pal <>
2025-07-15 01:09:09 +0530

    Red: Add test for newlines between numbers

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -25,4 +25,9 @@ describe("StringCalculator", () => {
     expect(calculator.add("1,2,3")).toBe(6);
     expect(calculator.add("1,2,3,4,5")).toBe(15);
   });
+
+  test("should handle newlines between numbers", () => {
+    expect(calculator.add("1\n2,3")).toBe(6);
+    expect(calculator.add("1\n2\n3")).toBe(6);
+  });
 });
